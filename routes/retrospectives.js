28219cf5d8ea67be80deb58b74bd8b5c87627545
Matheus Guilarducci <mguilarducci@gmail.com>
2014-03-26 17:03:37 -0300

    post retrospectives

--- routes/retrospectives.js
+++ routes/retrospectives.js
@@ -2,3 +2,8 @@ app.get('/retrospectives', function(req, res) {
     res.writeHead(200, { 'Content-Type': 'application/json' })
     res.end()
 })
+
+app.post('/retrospectives', function(req, res) {
+    res.writeHead(200)
+    res.end()
+})
--- test/test-retrospectives.js
+++ test/test-retrospectives.js
@@ -15,41 +15,47 @@ describe('Retrospectives', function() {
 	})
 
 	describe('Creating a retrospective', function() {
-		var data;
-		before(function(done) {
-	        if (mongoose.connection.db) {
-	            return done();
-	        }
-	        mongoose.connect(config.db, done)
-	    });
-
-	    after(function(done) {
-	        mongoose.connection.db.dropDatabase(function(){
-	            mongoose.connection.close(done)
-	        })
-	    })
-
-	    beforeEach(function(done) {
-	    	data = {title: 'title', date: Date.now}
-	    	done()
-	    })
-
-		it('should have a title', function(done) {
-			data.title = null
-			var retrospective = new Retrospective(data)
-			retrospective.save(function (err, retrospective) {
-                err.should.not.be.null
-                done()
-            })
+		it('POST /retrospectives should return status code 200', function(done) {
+			request(app).post('/retrospectives').expect(200, done)
 		})
 
-		it('should have a date', function(done) {
-			data.date = null
-			var retrospective = new Retrospective(data)
-			retrospective.save(function (err, retrospective) {
-                err.should.not.be.null
-                done()
-            })
-		})
+		describe('Database', function() {
+			var data;
+			before(function(done) {
+		        if (mongoose.connection.db) {
+		            return done();
+		        }
+		        mongoose.connect(config.db, done)
+		    });
+
+		    after(function(done) {
+		        mongoose.connection.db.dropDatabase(function(){
+		            mongoose.connection.close(done)
+		        })
+		    })
+
+		    beforeEach(function(done) {
+		    	data = {title: 'title', date: Date.now}
+		    	done()
+		    })
+
+			it('should have a title', function(done) {
+				data.title = null
+				var retrospective = new Retrospective(data)
+				retrospective.save(function (err, retrospective) {
+	                err.should.not.be.null
+	                done()
+	            })
+			})
+
+			it('should have a date', function(done) {
+				data.date = null
+				var retrospective = new Retrospective(data)
+				retrospective.save(function (err, retrospective) {
+	                err.should.not.be.null
+	                done()
+	            })
+			})
+		})	
 	})
 })
