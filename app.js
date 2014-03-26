05846d63ec750ce6a5da539cbed8f43347bc91ec
Matheus Guilarducci <mguilarducci@gmail.com>
2014-03-26 17:04:11 -0300

    post retrospectives

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+var env = process.env.NODE_ENV || 'development',
+    config = require('./config')[env],
+    mongoose = require('mongoose')
+
+var connect = function () {
+    //var options = { server: { socketOptions: { keepAlive: 1 } } }
+    mongoose.connect(config.db)
+}
+connect()
+
+mongoose.connection.on('error', function (err) {
+    console.log(err)
+})
+
+mongoose.connection.on('disconnected', function () {
+    connect()
+})
+
 var express = require('express')
 app = express()
 
